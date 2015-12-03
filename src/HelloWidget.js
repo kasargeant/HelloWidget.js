63fda4eb9ceeb261809c0a80e9016549359be67c
Kyle Alexis Sargeant <kylealexis@users.noreply.github.com>
2015-12-03 07:53:45 +0100

    Extends widget with highly-advanced, cutting-edge, super-powered... basic arithmetic.

--- src/HelloWidget.js
+++ src/HelloWidget.js
@@ -1,13 +1,43 @@
+
+
 /**
- *
- * @param x
- * @param y
+ * The all-capable HelloWidget.
+ * @param {number} x
+ * @param {number} y
  * @constructor
  */
 function HelloWidget(x, y) {
     this.x = x;
     this.y = y;
 }
-HelloWidget.prototype.getArea = function() {
+/**
+ * Adds the two member numbers
+ * @returns {number}
+ */
+HelloWidget.prototype.add = function() {
+    return this.x + this.y;
+};
+/**
+ * Subtracts the two member numbers
+ * @returns {number}
+ */
+HelloWidget.prototype.subtract = function() {
+    return this.x + this.y;
+};
+/**
+ * Multiplies the two member numbers
+ */
+HelloWidget.prototype.devideArea  = function() {
+
+
+    var z;
+    return  this.x +this.y;
+};
+/**
+ * Multiplies the two member numbers
+ * @returns {number}
+ */
+HelloWidget.prototype.multiply = function() {
     return this.x * this.y;
 };
+
